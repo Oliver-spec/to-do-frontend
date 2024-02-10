96566d97ad90fe246aac56e64b6cf151d1586ff5
Oliver Y. Kui <oliverkui0324@outlook.com>
2024-02-10 18:16:16 +0800

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import {
   postEvent,
 } from "../services/functions";
 import { Navigate } from "react-router-dom";
+import { IoIosCheckmarkCircleOutline, IoIosClose } from "react-icons/io";
 
 function App() {
   const [events, setEvents] = useState([]);
@@ -20,67 +21,93 @@ function App() {
   }, []);
 
   return (
-    <>
-      {events.map((event) => (
-        <div key={event.id}>
-          <div>{event.name}</div>
-          <div>{event.date}</div>
-          <div>{event.status}</div>
-          <button
-            type="button"
-            onClick={() => deleteEvent(event.id, setEvents, setError)}
-          >
-            DELETE
-          </button>
-          <button
-            type="button"
-            onClick={() => flipStatus(event.id, setEvents, setError)}
-          >
-            DONE
-          </button>
-        </div>
-      ))}
-      <input
-        type="text"
-        placeholder="Name"
-        value={input.name}
-        onChange={(event) => {
-          const newInput = { ...input, name: event.target.value };
-          setInput(newInput);
-        }}
-      />
-      <input
-        type="date"
-        value={input.date}
-        onChange={(event) => {
-          const newInput = { ...input, date: event.target.value };
-          setInput(newInput);
-        }}
-      />
-      <button
-        type="button"
-        onClick={() => {
-          postEvent(input, setEvents, setError);
-          setInput({
-            name: "",
-            date: "",
-          });
-        }}
-      >
-        ADD
-      </button>
-      <div>
-        {error}
-        {error ? (
-          <button type="button" onClick={() => setError("")}>
-            CLOSE
-          </button>
-        ) : null}
+    <main className="flex flex-col">
+      <div className="flex flex-row gap-10 p-3 bg-slate-200">
+        <input
+          className="border-2 border-black rounded-2xl p-2 outline-none text-lg w-1/5"
+          type="text"
+          placeholder="Name"
+          value={input.name}
+          onChange={(event) => {
+            const newInput = { ...input, name: event.target.value };
+            setInput(newInput);
+          }}
+        />
+        <input
+          className="border-2 border-black rounded-2xl p-2 outline-none text-lg w-1/5"
+          type="date"
+          value={input.date}
+          onChange={(event) => {
+            const newInput = { ...input, date: event.target.value };
+            setInput(newInput);
+          }}
+        />
+        <button
+          className="border-2 border-black rounded-2xl p-2 w-1/12 hover:bg-white"
+          type="button"
+          onClick={() => {
+            postEvent(input, setEvents, setError);
+            setInput({
+              name: "",
+              date: "",
+            });
+          }}
+        >
+          ADD
+        </button>
+        {error && (
+          <div className="flex gap-16 border-4 border-red-600 rounded-xl p-2 bg-rose-200 text-xl">
+            {error}
+            <button type="button" onClick={() => setError("")}>
+              <IoIosClose size="28px" />
+            </button>
+          </div>
+        )}
       </div>
+
+      <div className="ml-10">
+        <div className="p-5 text-2xl font-bold">Events</div>
+        {events.map((event) => (
+          <div
+            className="flex flex-row gap-10 border-2 border-black rounded-xl p-3 w-5/6 bg-slate-200 mb-5"
+            key={event.id}
+          >
+            <div className="flex flex-col items-center justify-center text-xl">
+              <div>{event.name}</div>
+            </div>
+            <div className="flex flex-col items-center justify-center text-xl">
+              <div>{event.date}</div>
+            </div>
+            <div className="flex flex-col items-center justify-center text-xl">
+              <div>
+                {event.status === "done" ? (
+                  <IoIosCheckmarkCircleOutline size="28px" />
+                ) : null}
+              </div>
+            </div>
+            <div className="flex-1"></div>
+            <button
+              className="border-2 border-black rounded-xl p-2 hover:bg-white"
+              type="button"
+              onClick={() => flipStatus(event.id, setEvents, setError)}
+            >
+              DONE
+            </button>
+            <button
+              className="border-2 border-black rounded-xl p-2 hover:bg-white"
+              type="button"
+              onClick={() => deleteEvent(event.id, setEvents, setError)}
+            >
+              DELETE
+            </button>
+          </div>
+        ))}
+      </div>
+
       {error === "Invalid Token - Please Login" ? (
         <Navigate to="/login" />
       ) : null}
-    </>
+    </main>
   );
 }
 
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { login } from "../services/login";
 import { Navigate } from "react-router-dom";
+import { IoIosClose } from "react-icons/io";
 
 export default function Login() {
   const [password, setPassword] = useState("");
@@ -8,7 +9,7 @@ export default function Login() {
   const [loggedIn, setLoggedIn] = useState(false);
 
   return (
-    <div className="h-screen flex items-center justify-center">
+    <main className="h-screen flex items-center justify-center">
       <form
         className="flex flex-col gap-8 w-1/3 items-center justify-center"
         onSubmit={(e) => {
@@ -25,22 +26,25 @@ export default function Login() {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
+
         <button
           className="border-2 border-black rounded-2xl p-2 w-1/2 hover:bg-slate-200"
           type="submit"
         >
           LOGIN
         </button>
+
         {error && (
           <div className="flex gap-16 border-4 border-red-600 rounded-xl p-3 bg-rose-200 text-xl">
             <div>{error}</div>
             <button type="button" onClick={() => setError("")}>
-              X
+              <IoIosClose size="28px" />
             </button>
           </div>
         )}
       </form>
+
       {loggedIn ? <Navigate to={"/"} /> : null}
-    </div>
+    </main>
   );
 }
